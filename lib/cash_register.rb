e2e9fe8786369189dd09bbea7b0341b0398abb6f
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-08-08 05:14:05 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -7,7 +7,7 @@ class CashRegister
     @discount = discount
   end
   
-  def add_item(item, change, quantity = 1)
+  def add_item(item, change)
     @item = item
     @total = @total * change
   end
