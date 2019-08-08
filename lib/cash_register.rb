56a64b16d2beda9fabb3dc8947adfc5f65c60761
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-08-08 05:06:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,12 +1,12 @@
 class CashRegister
 
-  attr_accessor :total, :discount
+  attr_accessor :total, :discount, :item, :price, :quantity
 
   def initialize(discount = 0)
     @total = 0
     @discount = discount
   
-  def add_item(item, price, )
+  def add_item(item, price, quantity = 0)
   
   end
 
